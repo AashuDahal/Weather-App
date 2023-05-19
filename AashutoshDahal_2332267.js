d05d8307a310c511032e90bfc86f62087c996099
AashuDahal <119505312+AashuDahal@users.noreply.github.com>
2023-05-19 10:09:16 +0545

    AashutoshDahal_2332267.js

--- AashutoshDahal_2332267.js
+++ AashutoshDahal_2332267.js
@@ -6,7 +6,7 @@ const forminput = document.getElementById('forminput');
 const cityname=document.getElementById('cityname');
 //function to get data of the needed city
 async function getWeather(city) {
-  try {
+ try{
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);//weather API URL
     const data = await response.json();//extracts the information from API and changes it into JSON format
     const temperature = `${data.main.temp} °C`;
@@ -15,15 +15,12 @@ async function getWeather(city) {
     const pressure= `${data.main.pressure} atm`;
     const weatherdescription = data.weather[0].description;
     const iconCode = data.weather[0].icon;//icon code for the weather icon
-    const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;//weather image URL
-    const bckgrndImg=`https://source.unsplash.com/1600x900/?${city}`;
-    
-    // Get current date and time
+    const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;//weather image URL    
+    // Get current date 
     const now = new Date();
     const day=['SUNDAY','MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY'];//defining array days
     const days=day[now.getDay()];//days of the week
     const date = now.toLocaleDateString();//local date
-    const time = now.toLocaleTimeString([],{hour:'2-digit',minute:'2-digit'});//local time 
 
     //this sends the data to the HTML to the id defined in the HTML
     document.getElementById('weathericon').src = iconUrl;
@@ -31,18 +28,17 @@ async function getWeather(city) {
     document.getElementById('cityname').innerHTML=city.toUpperCase();
     document.getElementById('date').innerHTML = date;
     document.getElementById('days').innerHTML = days;
-    document.getElementById('time').innerHTML= time;
     document.getElementById('temperature').innerHTML = temperature;
-    document.getElementById('humidity').innerHTML = humidity;
-    document.getElementById('windspeed').innerHTML = windSpeed;
-    document.getElementById('pressure').innerHTML = pressure;
-    document.body.style.backgroundimage=`url(${bckgrndImg})`;
-  } 
-  //handles the error
-    catch (error) {
-    alert("Error check your input/code");
+    document.getElementById('humiditydata').innerHTML = humidity;
+    document.getElementById('windspeeddata').innerHTML = windSpeed;
+    document.getElementById('pressuredata').innerHTML = pressure;
   }
-}
+    //handles the error
+    catch (error) {
+      alert("Error check your input/code");
+    }
+  }
+
 //Listen for form submission events and prevent the form from submitting in the default way
 form.addEventListener('submit', (event) => {
   event.preventDefault();//prevents the page from loading when the form is submitted
